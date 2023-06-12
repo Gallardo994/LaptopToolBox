ec6b1ecba8b99f88a3093e7a3c07a8e58ee87daa
Gallardo994 <evolutionv8@yandex.ru>
2023-06-12 05:28:17 +0400

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -60,24 +60,6 @@ namespace GHelper
                 var core = kernel.Get<ICoreRunner>();
                 core.Run(args);
 
-                /*
-
-                string language = AppConfig.GetString("language"); // TODO: Move to IAppConfig
-
-                if (language != null && language.Length > 0)
-                    Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(language);
-                else
-                {
-                    var culture = CultureInfo.CurrentUICulture;
-                    if (culture.ToString() == "kr") culture = CultureInfo.GetCultureInfo("ko");
-                    Thread.CurrentThread.CurrentUICulture = culture;
-                }
-
-                Debug.WriteLine(CultureInfo.CurrentUICulture);
-                */
-
-                // ProcessHelper.CheckAlreadyRunning();
-
                 var acpiChecker = kernel.Get<IAsusAcpiErrorProvider>();
 
                 if (!acpiChecker.InvokeCheckAndNotify())
