f469a269809a7e848fa5becfe4994573551b3a7d
Gallardo994 <evolutionv8@yandex.ru>
2023-06-12 06:09:40 +0400

    Update Toast.cs

--- app/Toasts/Toast.cs
+++ app/Toasts/Toast.cs
@@ -1,15 +1,20 @@
+using Ninject;
+
 namespace GHelper.Toasts;
 
 public class Toast : IToast
 {
-    public Toast()
-    {
-        
-    }
+    private readonly IToastIconResolver _iconResolver;
     
+    [Inject]
+    public Toast(IToastIconResolver iconResolver)
+    {
+        _iconResolver = iconResolver;
+    }
+
     public void Show(string text, ToastIconType iconType)
     {
-        
+        var iconBitmap = _iconResolver.ResolveIcon(iconType);
     }
 
     public void Hide()
