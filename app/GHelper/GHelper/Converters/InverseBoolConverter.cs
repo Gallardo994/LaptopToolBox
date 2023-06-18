3a8edca7b082db8f8835715059174255a50f0741
Gallardo994 <evolutionv8@yandex.ru>
2023-06-18 07:04:19 +0400

    Show error panel if performance control is unavailable

---
+++ app/GHelper/GHelper/Converters/InverseBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace GHelper.Converters;
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+
+        return value;
+    }
+}
