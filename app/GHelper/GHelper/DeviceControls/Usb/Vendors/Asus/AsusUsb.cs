1ec67fdf33b97556d7720fcf75d353fd2be35e0c
Gallardo994 <evolutionv8@yandex.ru>
2023-06-19 04:48:13 +0400

    Update AsusUsb.cs

--- app/GHelper/GHelper/DeviceControls/Usb/Vendors/Asus/AsusUsb.cs
+++ app/GHelper/GHelper/DeviceControls/Usb/Vendors/Asus/AsusUsb.cs
@@ -1,21 +1,9 @@
-﻿using Ninject;
-
-namespace GHelper.DeviceControls.Usb.Vendors.Asus;
+﻿namespace GHelper.DeviceControls.Usb.Vendors.Asus;
 
 public class AsusUsb : IUsb
 {
-    public int VendorId { get; init; }
-    public int[] DeviceIds { get; init; }
-    public byte LightingHidId { get; init; }
-    public byte InputHidId { get; init; }
-
-    [Inject]
-    public AsusUsb()
-    {
-        VendorId = 0x0b05;
-        DeviceIds = new []{ 0x1a30, 0x1854, 0x1869, 0x1866, 0x19b6, 0x1822, 0x1837, 0x1854, 0x184a, 0x183d, 0x8502, 0x1807, 0x17e0, 0x18c6 };
-        
-        LightingHidId = 0x5d;
-        InputHidId = 0x5a;
-    }
+    public int VendorId { get; } = 0x0b05;
+    public int[] DeviceIds { get; } = { 0x1a30, 0x1854, 0x1869, 0x1866, 0x19b6, 0x1822, 0x1837, 0x1854, 0x184a, 0x183d, 0x8502, 0x1807, 0x17e0, 0x18c6 };
+    public byte LightingHidId { get; } = 0x5d;
+    public byte InputHidId { get; } = 0x5a;
 }
