42241e9f1a4fb392a9c05fec917e8d9f6d560f43
Gallardo994 <evolutionv8@yandex.ru>
2023-06-22 01:27:22 +0400

    Prepare to create custom modes

--- app/GHelper/GHelper/DeviceControls/PerformanceModes/CustomPerformanceMode.cs
+++ app/GHelper/GHelper/DeviceControls/PerformanceModes/CustomPerformanceMode.cs
@@ -9,7 +9,7 @@ public class CustomPerformanceMode : IPerformanceMode
     [JsonProperty("id")] public Guid Id { get; set; }
     [JsonProperty("title")] public string Title { get; set; }
     [JsonProperty("description")] public string Description { get; set; }
-    [JsonIgnore] public string Icon { get; set; }
+    [JsonIgnore] public string Icon { get; } = "\uE7EE";
 
     [JsonIgnore] public PerformanceModeType Type { get; } = PerformanceModeType.Manual;
 }
--- app/GHelper/GHelper/DeviceControls/PerformanceModes/IPerformanceMode.cs
+++ app/GHelper/GHelper/DeviceControls/PerformanceModes/IPerformanceMode.cs
@@ -7,6 +7,6 @@ public interface IPerformanceMode
     public Guid Id { get; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Icon { get; set; }
+    public string Icon { get; }
     public PerformanceModeType Type { get; }
 }
