f29d0f82b57eab39adec17c71b0be74b02c3b189
Gallardo994 <evolutionv8@yandex.ru>
2023-06-24 23:37:58 +0400

    Update wallpaper in UI

--- app/GHelper/GHelper/ExtraControls/Wallpaper/IWallpaperProvider.cs
+++ app/GHelper/GHelper/ExtraControls/Wallpaper/IWallpaperProvider.cs
@@ -1,8 +1,11 @@
 ﻿using GHelper.Helpers;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace GHelper.ExtraControls.Wallpaper;
 
 public interface IWallpaperProvider : IObservableObject
 {
     public string ImagePath { get; }
+    public BitmapImage ImageSource { get; }
 }
