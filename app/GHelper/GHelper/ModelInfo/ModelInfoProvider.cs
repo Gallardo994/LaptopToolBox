c9562595b9590b69d3fa33ff1994aedfb29cb1d2
Gallardo994 <evolutionv8@yandex.ru>
2023-07-02 16:02:34 +0400

    Rework model info to MMI

--- app/GHelper/GHelper/ModelInfo/ModelInfoProvider.cs
+++ app/GHelper/GHelper/ModelInfo/ModelInfoProvider.cs
@@ -1,42 +1,52 @@
-﻿using System.Management;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GHelper.DeviceControls.Wmi;
 
-namespace GHelper.ModelInfo;
-
-public partial class ModelInfoProvider : ObservableObject, IModelInfoProvider
+namespace GHelper.ModelInfo
 {
-    [ObservableProperty] private string _model;
-    [ObservableProperty] private int _bios;
-
-    public ModelInfoProvider()
+    public partial class ModelInfoProvider : ObservableObject, IModelInfoProvider
     {
-        using var objSearcher = new ManagementObjectSearcher("SELECT SMBIOSBIOSVersion FROM Win32_BIOS");
-        using var objCollection = objSearcher.Get();
-        
-        foreach (ManagementObject obj in objCollection)
-        {
-            if (obj["SMBIOSBIOSVersion"] is null)
-            {
-                continue;
-            }
-                
-            var results = obj["SMBIOSBIOSVersion"].ToString().Split(".");
-                    
-            if (results.Length > 1)
-            {
-                Model = results[0];
-                if (int.TryParse(results[1], out var bios))
-                {
-                    Bios = bios;
-                }
-                return;
-            }
+        [ObservableProperty] private string _model;
+        [ObservableProperty] private int _bios;
 
-            Model = results[0];
+        public ModelInfoProvider(IWmiSessionFactory wmiSessionFactory)
+        {
+            using var session = wmiSessionFactory.CreateSession();
+            var instances = session.QueryInstances(@"root\cimv2", "WQL", "SELECT SMBIOSBIOSVersion FROM Win32_BIOS");
+
+            foreach (var instance in instances)
+            {
+                var biosVersionProp = instance.CimInstanceProperties["SMBIOSBIOSVersion"];
+                
+                if (biosVersionProp.Value is null)
+                {
+                    continue;
+                }
+                
+                var stringVal = biosVersionProp.Value.ToString();
+                
+                if (string.IsNullOrEmpty(stringVal))
+                {
+                    continue;
+                }
+                
+                var results = stringVal.Split(".");
+                
+                if (results.Length > 1)
+                {
+                    Model = results[0];
+                    if (int.TryParse(results[1], out var bios))
+                    {
+                        Bios = bios;
+                    }
+                    return;
+                }
+
+                Model = results[0];
+                Bios = 0;
+            }
+            
+            Model = string.Empty;
             Bios = 0;
         }
-        
-        Model = string.Empty;
-        Bios = 0;
     }
 }
